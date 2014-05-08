c4c0672887af975d3b6f231726fb6972be59b24d
Joe Osborne <josborne@haywirelabs.com>
2014-05-08 10:28:07 -0400

    spawn zap process

--- test/test.js
+++ test/test.js
@@ -5,8 +5,6 @@ var SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
 var test = require('selenium-webdriver/testing');
 var pathToSeleniumJar = 'server/selenium-server-standalone-2.41.0.jar';
 var homepageUrl = 'https://dev.healthcare.gov/?ACA=9xCVchyHgWzc';
-var pathToZapJar = 'server/zap/ZAP_2.3.0.1/zap.jar';
-var pathToZapSh = 'server/zap/ZAP_2.3.0.1/zap.sh';
 
 var server = new SeleniumServer(pathToSeleniumJar, {
   port: 4444,
